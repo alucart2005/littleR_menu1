fb9c24f42dede655755ba85783d43e607a37c750
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-12-17 16:26:12 -0500

    Feature: Map li

--- src/Routers/routes.jsx
+++ src/Routers/routes.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "../Pages/Home";
 import { NavBar } from "../Components/NavBar";
+import { Home } from "../Pages/Home";
+import { Products } from "../Pages/Products";
+import { Reports } from "../Pages/Reports";
 
 export function MyRoutes() {
   return (
     <BrowserRouter>
-        <NavBar/>
+      <NavBar />
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/products" element={<Products />} />
+        <Route exact path="/Reports" element={<Reports />} />
       </Routes>
     </BrowserRouter>
   );
